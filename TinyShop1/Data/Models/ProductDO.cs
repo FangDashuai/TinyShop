6fe8c706e6ccedd9092cafcf7dac31d961e9df6d
FangHaoyi <1198264408@qq.com>
2020-06-14 12:57:18 +0800

    Tiny Shop

---
+++ TinyShop1/Data/Models/ProductDO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Data.Models
+{
+    public class ProductDO
+    {
+
+        [Key] public long ID { get; set; }
+        //产品编号
+        [StringLength(100)] public string ProductNmuber { get; set; }
+        //产品名称
+        [StringLength(100)] public string ProductName { get; set; }
+        //产品类型
+        [StringLength(100)] public string ProductType { get; set; }
+        //产品价格
+        [StringLength(100)] public double Price { get; set; }
+    }
+}
---
+++ TinyShop1/TinyShop1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TinyShop1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
